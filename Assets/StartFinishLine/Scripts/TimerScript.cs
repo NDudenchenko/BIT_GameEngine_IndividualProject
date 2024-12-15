b33f350c963415e64c4533fcefa7b044f7412745
NDudenchenko <n.dudenchenko@gmail.com>
2024-12-15 02:50:39 +0200

    - Added timer with collision of the start and finish

---
+++ Assets/StartFinishLine/Scripts/TimerScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    public TimerCollider StartCollider;
+    public TimerCollider FinishCollider;
+    public TextMeshProUGUI TimerText;
+
+    private float secondsCount;
+    private int minuteCount;
+    private bool isCounterWorks = false;
+
+    void Start()
+    {
+        StartCollider.CollideWithPlayer += () => OnStartColliderTriggered();
+        FinishCollider.CollideWithPlayer += () => OnFinishColliderTriggered();
+    }
+
+    void Update()
+    {
+        if (isCounterWorks)
+        {
+            secondsCount += Time.deltaTime;
+            if (secondsCount >= 60)
+            {
+                minuteCount++;
+                secondsCount = 0;
+            }
+
+            Debug.Log(minuteCount + "m:" + (int)secondsCount + "s");
+
+            TimerText.text = minuteCount + "m:" + (int)secondsCount + "s";
+        }
+        else
+        {
+            secondsCount = 0;
+            minuteCount = 0;
+        }
+    }
+    public void OnColliderTriggered(Collider other)
+    {
+        if (other == StartCollider)
+        {
+
+        }
+    }
+
+    public void OnStartColliderTriggered()
+    {
+        isCounterWorks = true;
+    }
+
+    public void OnFinishColliderTriggered()
+    {
+        isCounterWorks = false;
+    }
+}
